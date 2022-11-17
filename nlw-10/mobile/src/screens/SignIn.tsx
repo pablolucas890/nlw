27731a81db5a9272f7c42b2f30008d9b9d1f2b8f
pablolucas890 <pablolucas890@gmail.com>
2022-11-17 13:50:24 -0300

    30 min aula 3

--- nlw-10/mobile/src/screens/SignIn.tsx
+++ nlw-10/mobile/src/screens/SignIn.tsx
@@ -1,11 +1,19 @@
-import { Center, StatusBar, Text } from 'native-base';
+import { Center, StatusBar, Icon } from 'native-base';
+import { Fontisto } from '@expo/vector-icons'
+
 import Logo from '../assets/logo.svg'
+import { Button } from '../components/Button';
 
 export function SignIn() {
     return (
         <Center flex={1} bgColor="gray.900" alignItems={"center"} justifyContent="center">
-            <Logo />
             <StatusBar barStyle={'default'} />
+            <Logo width={212} height={40} />
+            <Button
+                type='SECONDARY'
+                title={"Entrar com Google"}
+                leftIcon={<Icon as={Fontisto} name="google" color={"white"} size="md" />}
+            />
         </Center>
     )
 }
